3181f79da97d9afa52137f88d07189ce8e05cbe2
matias-altamirano <matias7238@gmail.com>
2023-08-21 16:22:01 -0300

    agregando

---
+++ prestamos.js
@@ -0,0 +1,28 @@
+
+
+
+function calcular_prestamo(monto , cuotas ){
+
+    monto = parseFloat(monto);
+    cuotas = parseInt(cuotas);
+
+    let prestamo = 0;
+
+    if(cuotas == 1 && monto > 0){
+
+    }
+}
+
+
+
+// Prestamos //
+
+console.log("Bienvenido/a a Prestamos El PEPE");
+
+let monto = 0;
+
+while( monto != "FIN") {
+
+    monto = prompt("Ingrese el monto deseado o FIN");
+    let cuotas = prompt("Ingrese la cantidad de cuotas: 1, 3, 6 y 12");
+}
